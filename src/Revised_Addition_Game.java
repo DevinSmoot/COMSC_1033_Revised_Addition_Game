af0832f47155d35527ec2655992e1a05df1b3910
Devin Smoot <smootd@student.swosu.edu>
2015-11-13 00:39:46 -0600

    Removed debug code and cleaned it up

--- src/Revised_Addition_Game.java
+++ src/Revised_Addition_Game.java
@@ -7,23 +7,12 @@
 import java.util.Scanner;
 
 public class Revised_Addition_Game {
-	//Define globals
-	//public static int number1;
-	//public static int number2;
-	//public static int hardness = 10;
-	//public static int score;
-	//public static int userAnswer;
-	//public static int problemAnswer;
-	//dummy comment
-	
 	public static void main(String[] args) {
-		//System.out.println("Main method"); //Debug statement
 		//Call the addition game
 		additionGameMethod();
 	}//End main
 
 	public static void additionGameMethod() {
-		//System.out.println("Addition Game Method"); //Debug statement
 		//Define local variables
 		int number1;
 		int number2;
@@ -38,7 +27,6 @@ public class Revised_Addition_Game {
 		System.out.println ("If a question is answered incorrectly the difficulty will decrease.");
 				
 		for(int i=0;i<4;i=i+1){
-			//System.out.println ("FOR loop"); //Debug statement
 			//Ask the user to add these two numbers together
 			
 			number1 = randomNumber(hardness);
@@ -48,8 +36,9 @@ public class Revised_Addition_Game {
 			problemAnswer = number1 + number2; 
 			//System.out.println ("Answer: " + problemAnswer); //Debug statement
 			
-			//Ask the user to add these two numbers together
+			//Round information
 			System.out.println("\nRound #" + (i + 1));
+			//Ask user to answer problem
 			System.out.println("What is " + number1 + " + " + number2 + "?");
 			//Read in their response
 			Scanner input = new Scanner(System.in);
@@ -65,7 +54,6 @@ public class Revised_Addition_Game {
 			 if (isAnswerCorrect(userAnswer, problemAnswer) == true){
 				 	System.out.println("The answer was correct.");
 					System.out.println("\n2 points added and difficulty increased.");
-					//System.out.println("Hardness of question: " + hardness); //Debug statement
 					hardness *= 10;
 					score += 2;
 					System.out.println("Score: " + score);
@@ -73,13 +61,11 @@ public class Revised_Addition_Game {
 				 	System.out.println("The answer was incorrect.");
 					System.out.println("The correct answer was: " + problemAnswer); //Can be removed if you wish
 					System.out.println("\n1 point removed and difficulty decreased.");
-					//System.out.println("Hardness of question: " + hardness); //Debug statement
 					if (hardness <= 10){
 						hardness = 10;
 					}else{
 						hardness /= 10;
 					}
-					//System.out.println("Hardness of question: " + hardness); //Debug statement
 					score -= 1;
 					System.out.println("Score: "+ score);
 			 }
@@ -90,14 +76,11 @@ public class Revised_Addition_Game {
 	}//End additionGameMethod
 	
 	public static int randomNumber(int hardness){
-		//System.out.println ("Random number method"); //Debug statement
 		int generatedNumber = (int)(Math.random()*hardness);
-		//System.out.println ("Random number: " + generatedNumber); //Debug statement
 		return generatedNumber;
 	}//End randomNumber
 	
 	public static boolean isAnswerCorrect(int userAnswer, int problemAnswer){
-		//System.out.println("Is Answer Correct method"); //Debug statement
 		boolean answerCorrect;
 				if (userAnswer == problemAnswer){
 					return true;
